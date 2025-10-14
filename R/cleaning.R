e3bfcfa4acff8a7e5c12e95a249b82f4e55b06eb
Loïc <loic.memeteau@kedgebs.com>
2025-10-14 10:16:42 +0200

    modif cleaning.R

--- R/cleaning.R
+++ R/cleaning.R
@@ -1,3 +1,4 @@
+library(dplyr)
 file <- "data_raw/ds_salaries.csv"
 
 data <- read.csv2(file, header = TRUE, stringsAsFactors = FALSE, na.strings = c("", "NA"), fileEncoding = "UTF-8")
@@ -6,6 +7,14 @@ data <- read.csv2(file, header = TRUE, stringsAsFactors = FALSE, na.strings = c(
 print(names(data))
 data <- data.frame(data)
 data <- data[, -5]
+
+df <- data %>%
+  mutate(salary_in_usd = as.double(salary_in_usd),
+         remote_ratio = as.integer(remote_ratio),
+         experience_level = factor(experience_level, levels = "EN", "MI", "SE", "EX"),
+         employment_type = factor(employment_type, levels = "PT", "CT", "FT", "FL"),
+         company_size = factor(company_size, levels = "S", "M", "L"))
+
 
 data_2020 <- subset(data, work_year == 2020)
 data_2021 <- subset(data, work_year == 2021)
@@ -16,4 +25,3 @@ print(nrow(data_2020))
 print(nrow(data_2021))
 print(nrow(data_2022))
 print(nrow(data_2023))
-
