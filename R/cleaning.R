fb1d40b2506ea04b6e34bd851b542026fc022175
Loïc <loic.memeteau@kedgebs.com>
2025-10-14 08:51:49 +0200

    Début nettoyage des données

--- R/cleaning.R
+++ R/cleaning.R
@@ -1,4 +1,4 @@
-data <- read.csv2("/Users/loic/Documents/UGA/Logiciel Spécialisé/Projet_R/projet-R-Julia/data_raw/ds_salaries.csv")
+data <- read.csv2()("/Users/loic/Documents/UGA/Logiciel Spécialisé/Projet_R/projet-R-Julia/data_raw/ds_salaries.csv")
 print(data)
 print(row.names(data))
 data <- data.frame(data)
