ab415a07326dfb514ea09db03340b959ffc74998
Loïc <loic.memeteau@kedgebs.com>
2025-10-14 09:03:15 +0200

    Début nettoyage des données

--- R/cleaning.R
+++ R/cleaning.R
@@ -3,11 +3,8 @@ file <- "/Users/loic/Documents/UGA/Logiciel Spécialisé/Projet_R/projet-R-Julia
 data <- read.csv2(file, header = TRUE, stringsAsFactors = FALSE, na.strings = c("", "NA"), fileEncoding = "UTF-8")
 
 
-# Quick diagnostics
-
 print(names(data))
 data <- data.frame(data)
-
 
 data_2020 <- subset(data, work_year == 2020)
 data_2021 <- subset(data, work_year == 2021)
@@ -16,4 +13,5 @@ data_2023 <- subset(data, work_year == 2023)
 
 print(nrow(data_2020))
 print(nrow(data_2021))
-
+print(nrow(data_2022))
+print(nrow(data_2023))
