c12ac20421e71ea9cebd484059a0db8e97b15e77
Loïc <loic.memeteau@kedgebs.com>
2025-10-14 10:23:29 +0200

    modif cleaning.R

--- R/cleaning.R
+++ R/cleaning.R
@@ -7,14 +7,18 @@ data <- read.csv2(file, header = TRUE, stringsAsFactors = FALSE, na.strings = c(
 print(names(data))
 data <- data.frame(data)
 data <- data[, -5]
+sum(is.na(as.double(data$salary_in_usd))) 
 
 df <- data %>%
   mutate(salary_in_usd = as.double(salary_in_usd),
          remote_ratio = as.integer(remote_ratio),
-         experience_level = factor(experience_level, levels = "EN", "MI", "SE", "EX"),
-         employment_type = factor(employment_type, levels = "PT", "CT", "FT", "FL"),
-         company_size = factor(company_size, levels = "S", "M", "L"))
+         experience_level = factor(experience_level, levels = c("EN", "MI", "SE", "EX")),
+         employment_type = factor(employment_type, levels = c("PT", "CT", "FT", "FL")),
+         company_size = factor(company_size, levels = c("S", "M", "L")),
+         work_year = factor(work_year, levels = c(2020, 2021, 2022, 2023))
+  )
 
+head(df)
 
 data_2020 <- subset(data, work_year == 2020)
 data_2021 <- subset(data, work_year == 2021)
