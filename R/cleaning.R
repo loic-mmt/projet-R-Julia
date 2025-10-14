28d65863dfb3471e1ce006dba47a4dd40e6fc95e
Loïc <loic.memeteau@kedgebs.com>
2025-10-14 08:51:18 +0200

    Début nettoyage des données

--- R/cleaning.R
+++ R/cleaning.R
@@ -1,4 +1,8 @@
-data <- read.csv("/Users/loic/Documents/UGA/Logiciel Spécialisé/Projet_R/projet-R-Julia/data_raw/ds_salaries.csv")
+data <- read.csv2("/Users/loic/Documents/UGA/Logiciel Spécialisé/Projet_R/projet-R-Julia/data_raw/ds_salaries.csv")
 print(data)
+print(row.names(data))
+data <- data.frame(data)
 
-data_2021 <- data$work_year["2022"]
+print(data[3:8, 4])
+data_2021 <- subset(data, work_year == "2021")
+print(data_2021)
