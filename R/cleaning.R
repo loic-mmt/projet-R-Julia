a02743d91d85f6ef0c117199f8ebcd527207b296
Loïc <loic.memeteau@kedgebs.com>
2025-10-14 09:00:16 +0200

    Début nettoyage des données

--- R/cleaning.R
+++ R/cleaning.R
@@ -1,32 +1,19 @@
 file <- "/Users/loic/Documents/UGA/Logiciel Spécialisé/Projet_R/projet-R-Julia/data_raw/ds_salaries.csv"
-# Peek first line to guess separator
-first <- readLines(file, n = 1, warn = FALSE)
-sep <- if (grepl(";", first)) ";" else if (grepl("\t", first)) "\t" else ","
 
-# Read with the right reader
-if (sep == ";") {
-  data <- read.csv2(file, header = TRUE, stringsAsFactors = FALSE, na.strings = c("", "NA"), fileEncoding = "UTF-8")
-} else if (sep == "\t") {
-  data <- read.delim(file, header = TRUE, stringsAsFactors = FALSE, na.strings = c("", "NA"), fileEncoding = "UTF-8")
-} else {
-  data <- read.csv(file, header = TRUE, stringsAsFactors = FALSE, na.strings = c("", "NA"), fileEncoding = "UTF-8")
-}
+data <- read.csv2(file, header = TRUE, stringsAsFactors = FALSE, na.strings = c("", "NA"), fileEncoding = "UTF-8")
+
 
 # Quick diagnostics
-cat("Separator detected:", if (sep == "\t") "[TAB]" else sep, "\n")
-cat("Dimensions:", paste(dim(data), collapse = " x "), "\n")
+
 print(names(data))
-head(data, 3)
-print(data)
 data <- data.frame(data)
 
-if ("work_year" %in% names(data)) {
-  if (is.numeric(data$work_year)) {
-    data_2021 <- subset(data, work_year == 2021)
-  } else {
-    data_2021 <- subset(data, work_year == "2021")
-  }
-  print(data_2021)
-} else {
-  warning("La colonne 'work_year' est introuvable dans les données importées.")
-}
+
+data_2020 <- subset(data, work_year == 2020)
+data_2021 <- subset(data, work_year == 2021)
+data_2022 <- subset(data, work_year == 2022)
+data_2023 <- subset(data, work_year == 2023)
+
+print(nrow(data_2020))
+print(nrow(data_2021))
+
