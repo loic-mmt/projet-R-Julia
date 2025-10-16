f408fe47df6c97711d0f1cfd2c58bfd9fd38f6b5
Arthur <hauguel.arthur@gmail.com>
2025-10-16 10:44:28 +0200

    explication des fonctions

---
+++ packages/PackageRClean/R/utils.R
@@ -0,0 +1,15 @@
+#' Say hello
+#'
+#' A simple example function that greets a person.
+#'
+#' @param name A character string with the person's name.
+#'
+#' @return A character string saying hello.
+#' @export
+#'
+#' @examples
+#' say_hello("Alice")
+say_hello <- function(name) {
+  paste("Hello,", name, "!")
+}
+devtools::document("PackageRClean")
