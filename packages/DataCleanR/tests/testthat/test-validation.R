92323c61a00dcfd35bb566000319392b8c7279ed
Loïc <loic.memeteau@kedgebs.com>
2025-10-21 10:57:54 +0200

    explication des fonctions

--- packages/DataCleanR/tests/testthat/test-validation.R
+++ packages/DataCleanR/tests/testthat/test-validation.R
@@ -1,5 +1,5 @@
 test_that("read_raw_csv importe correctement", {
-  df <- data.frame(a= 1:3, b=c ("x", "y", "z"))
+  df <- data.frame(a = 1:3, b = c("x", "y", "z"))
   tmp <- tempfile(fileext = ".csv")
   write.csv(df, tmp, row.names = FALSE)
   imported_df <- read_raw_csv(tmp)
