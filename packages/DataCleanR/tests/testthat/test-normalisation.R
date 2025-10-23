366feb98861f734a7a818193ffc53681d5a80d47
Arthur <hauguel.arthur@gmail.com>
2025-10-23 12:51:21 +0200

    ajout fonction dans normalisation

--- packages/DataCleanR/tests/testthat/test-normalisation.R
+++ packages/DataCleanR/tests/testthat/test-normalisation.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+test_that("normalize_factor", {
+  mapping_test <- c(
+    "FT" = "Full-time",
+    "PT" = "Part-time",
+    "CT" = "Contract",
+    "FL" = "Freelance"
+  )
+  codes_test <- c("FT", "PT", "FL", "CT", "PT", "XX")
+  levels_test = c("Full-time", "Part-time", "Contract", "Freelance", "Unknown")
+  result <- normalize_factor(codes_test, mapping_test, levels_test)
+  expect_s3_class(result, "factor")
+  expect_equal(as.character(result), c("Full-time" "Part-time" "Freelance" "Contract"  "Part-time" "Unknown"))
+  expect_equal(levels(result), levels_test)
+  })
