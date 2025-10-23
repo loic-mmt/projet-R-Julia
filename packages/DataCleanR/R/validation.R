42245bf3d361e6c4534e4ca9e7e5c98ae8bdf887
Loïc <loic.memeteau@kedgebs.com>
2025-10-23 08:54:24 +0200

    ajout fonction dans validation

--- packages/DataCleanR/R/validation.R
+++ packages/DataCleanR/R/validation.R
@@ -28,6 +28,7 @@ to_snake_case <- function(data) {
   data <- gsub(" ", "_", data)
   data <- gsub("-", "_", data)
   data <- gsub("__+", "_", data)
+  data <- gsub("([a-z])([A-Z])", "\\1_\\2", data)
   data <- gsub("[^a-z0-9_]", "", data)
   data <- tolower(data)
   return(data)
