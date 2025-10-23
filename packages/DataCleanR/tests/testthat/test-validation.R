597ac2d42095767ba84639b83f34e4ac4decce14
Arthur <hauguel.arthur@gmail.com>
2025-10-23 09:59:05 +0200

    ajout fonction dans validation

--- packages/DataCleanR/tests/testthat/test-validation.R
+++ packages/DataCleanR/tests/testthat/test-validation.R
@@ -7,6 +7,15 @@ test_that("read_raw_csv importe correctement", {
   expect_true(is.data.frame(imported_df))
 })
 
+test_that("validate_schema", {
+  df <- data.frame(a = 1:3, b = c("x", "y", "z"))
+  names_to_test1 <- c("a", "c")
+  names_to_test2 <- c("a", "b", "c")
+  test1 <- validate_schema(df, names_to_test1))
+  test2 <- validate_schema(df, names_to_test2))
+  expect_equal(list(test1, test2), list("All required colums are present in the dataframe", "The dataframe in not complete and it's missing c"))
+})
+
 test_that("to_snake_case", {
   df <- data.frame("Nom Client" = c("Alice", "Bob"),
                    "MontantTotal" = c(100, 200),
@@ -15,12 +24,3 @@ test_that("to_snake_case", {
   expect_equal(names(df), c("nom_client", "montant_total", "date_de_vente"))
 })
 
-test_that("validate_schema", {
-  df <- data.frame(a = 1:3, b = c("x", "y", "z"))
-  names_to_test1 <- c("a", "c")
-  names_to_test2 <- c("a", "b", "c")
-  test1 <- validate_schema(df, names_to_test1))
-  test2 <- validate_schema(df, names_to_test2))
-  expect_equal(list(test1, test2), list("All required colums are present in the dataframe", "The dataframe in not complete and it's missing c"))
-}
-)
