c959e11d94eb7e6ea273f85c1b4393855e59bcf4
Arthur <hauguel.arthur@gmail.com>
2025-10-23 11:04:10 +0200

    ajout fonction dans normalisation

--- packages/DataCleanR/R/normalisation.R
+++ packages/DataCleanR/R/normalisation.R
@@ -2,11 +2,13 @@
 #'
 #' @param vector the vector to normalize 
 #' @param mapping the mapping to indicate which values to transform in which.
+#' @param levels_in_factor les niveaux à indiquer
+#' @param ordered_factor Indicate if the vector has to be ordonned, FALSE by default
 #' @return normalized the normalized vector
 #' @export
-normalize_factor <- function(vector, mapping, levels_in_factor) {
+normalize_factor <- function(vector, mapping, levels_in_factor, ordered_factor = FALSE) {
   normalized <- mapping[vector]
   normalized[is.na(normalized)] <- "Unknown"
-  normalized <- factor(normalized, levels = levels_in_factor)
+  normalized <- factor(normalized, levels = levels_in_factor, ordered = ordered_factor)
   return(normalized)
 }
