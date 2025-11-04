c7245d2dde52daca1dcb8ae316443350a06259c0
Loïc <loic.memeteau@kedgebs.com>
2025-11-04 09:28:47 +0100

    ajout fonction dans normalisation

--- packages/DataAnalysisR/tests/testthat/test-hypothesis_testing.R
+++ packages/DataAnalysisR/tests/testthat/test-hypothesis_testing.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+testthat("variance_homogeneity_test - levene works", {
+  df <- data.frame(company_location = c("US", "US", "US", "FR"),
+                   salary_in_usd = c(100, 200, 1000, 2000),
+                   stringsAsFactors = FALSE)
+
+    res <- variance_homogeneity_test(data, y = "salary_in_usd", group = "company_location", method = "levene")
+    testthat::expect_s3_class(res, "anova")
+    testthat::expect_true("Pr(>F)" %in% colnames(res))
+    p <- res["g", "Pr(>F)"]
+    testthat::expect_true(is.numeric(p) && !is.na(p))
+    testthat::expect_gt(p, 0.05)
 })
