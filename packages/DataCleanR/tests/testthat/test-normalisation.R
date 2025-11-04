e9eb358b9891c1c7e34fd53f35915eb508705bb1
MargotL3869 <margot.laignel8@gmail.com>
2025-11-04 10:52:21 +0100

    ajout fonction dans normalisation

--- packages/DataCleanR/tests/testthat/test-normalisation.R
+++ packages/DataCleanR/tests/testthat/test-normalisation.R
@@ -1,11 +1,18 @@
 test_that("normalize_experience_level works", {
-  df <- data.frame(experience_level = c("MI", "SE", "EN", "EX", "MI"), stringsAsFactors = FALSE)
-  res <- normalize_experience_level(df)
+  df <- data.frame(experience_level = c("MI","SE","EN","EX","MI"), stringsAsFactors = FALSE)
+  res <- normalize_ordered_column(df, "experience_level", c("EN","MI","SE","EX"))
 
   expect_s3_class(res$experience_level, "ordered")
-  expect_equal(levels(res$experience_level), c("EN", "MI", "SE", "EX"))
-  expect_equal(as.character(res$experience_level), c("MI", "SE", "EN", "EX", "MI"))
+  expect_equal(levels(res$experience_level), c("EN","MI","SE","EX"))
+  expect_equal(as.character(res$experience_level), c("MI","SE","EN","EX","MI"))
+
+  df_invalid <- data.frame(experience_level = c("XX","MI"))
+  res_invalid <- normalize_ordered_column(df_invalid, "experience_level", c("EN","MI","SE","EX"))
+  expect_true(is.na(res_invalid$experience_level[1]))
+
+  expect_error(normalize_ordered_column(df, "unknown", c("EN","MI","SE","EX")))
 })
+
 
 test_that("normalize_remote_ratio works correctly", {
 
